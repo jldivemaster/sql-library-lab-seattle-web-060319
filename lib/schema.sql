5b0dc409374268b0f88054ff436e6a89bef53686
Jason Leach <jlspursfan@gmail.com>
2019-06-13 02:02:18 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,14 +1,14 @@
-CREATE TABLE series (
+"CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title TEXT,
   author_id INTEGER,
   sub_genre_id INTEGER
-)
+)"
 
-CREATE TABLE sub_genres (
+"CREATE TABLE sub_genres (
   id INTEGER PRIMARY KEY,
   name TEXT
-)
+)"
 
 CREATE TABLE authors (
   id INTEGER PRIMARY KEY,
