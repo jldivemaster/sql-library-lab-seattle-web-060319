5d5228cac8de1dd3a433b3cdae50c3b6b1c78887
Jason Leach <jlspursfan@gmail.com>
2019-06-13 02:35:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenre.name FROM (SELECT subgenre.name, series.author_id FROM subgenres JOIN series WHERE series.subgenre_id = sub-genre.id) JOIN authors WHERE author.id = series.author_id"
+  "SELECT authors.name, subgenre.name FROM (SELECT subgenres.name, series.author_id FROM subgenres JOIN series WHERE series.subgenre_id = sub-genre.id) JOIN authors WHERE author.id = series.author_id"
 end
 
 def select_series_title_with_most_human_characters
