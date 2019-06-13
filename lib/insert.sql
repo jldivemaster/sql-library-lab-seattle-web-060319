555177aac149d79a56e8b2450c5d031f3a8b3265
Jason Leach <jlspursfan@gmail.com>
2019-06-13 01:57:02 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -23,4 +23,19 @@ INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Mag
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Another Attractive & Intelligent Woman", "I can empower and objectify women at the same time!", "human", 1, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Professor Bumblebee", "Wizardbots, roll out!", "wizard", 2, 1);
 
-INSERT INTO character_books (character_id, book_id) VALUES ();
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 4);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 5);
+INSERT INTO character_books (character_id, book_id) VALUES (2, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (2, 3);
+INSERT INTO character_books (character_id, book_id) VALUES (2, 6);
+INSERT INTO character_books (character_id, book_id) VALUES (5, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (5, 4);
+INSERT INTO character_books (character_id, book_id) VALUES (5, 5);
+INSERT INTO character_books (character_id, book_id) VALUES (4, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (4, 3);
+INSERT INTO character_books (character_id, book_id) VALUES (4, 6);
+INSERT INTO character_books (character_id, book_id) VALUES (3, 6);
+INSERT INTO character_books (character_id, book_id) VALUES (6, 4);
+INSERT INTO character_books (character_id, book_id) VALUES (7, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (8, 3);
